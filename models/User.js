09adcaeaf121f8204ab408da90cd61e5aeddd811
gof9325 <gof9325@naver.com>
2021-06-14 21:16:54 +0900

    comend

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +32,34 @@ const userSchema = mongoose.Schema({
         type : Number
     }
 })
+
+userSchema.pre('save', function( next ){
+
+    var user = this;
+    if(user.isModified('password')){
+        //비밀번호를 암호화 시킨다.
+     bcrypt.genSalt(saltRounds, function(err, salt){
+
+        if(err) return next(err)
+        bcrypt.hash(user.password, salt, function(err, hash){
+            if(err) return next(err)
+            user.password = hash
+            next()
+        })
+    })
+    } else {
+        next()
+    }
+})
+
+userSchema.methods.compaterPassword = function(plainPasswrod, cb){
+
+    //plainPasswrod 123456  암호화된 비밀번호
+    bcrypt.compare(plainPasswrod, this.password, function(err, isMatch) {
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 module.exports = {User}
